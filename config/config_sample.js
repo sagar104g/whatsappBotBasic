f2fcfe3d6a574f5e6622713ec7ec70ac9a9a4316
sagar104g <sagar104gurgaon@gmail.com>
2020-08-08 16:48:58 +0530

    first commit

---
+++ config/config_sample.js
@@ -0,0 +1,8 @@
+var config = {
+	"authToken": 'zxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx',
+	"accountSid": 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx',
+	"PORT": 5000,
+	"sender": '9999999999'
+};
+
+module.exports = config;
